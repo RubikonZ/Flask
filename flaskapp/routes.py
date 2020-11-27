0a938e73a96f7f9cc9ada8d88e79b1e323b7fc17
Ruben <rubenzurr@gmail.com>
2020-11-27 15:59:01 +0300

    Added feature to display posts on home page, also ability to edit them as author and added author's avatar for each post

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,30 +1,18 @@
-from flask import Flask, render_template, flash, redirect, url_for, request
-from flaskapp.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flask import Flask, render_template, flash, redirect, url_for, request, abort
+from flaskapp.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskapp.models import User, Post
 from flaskapp import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 import secrets
 import os
-
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
+from PIL import Image
+from datetime import datetime
 
 
 @app.route('/home')
 @app.route('/')
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -74,15 +62,23 @@ def logout():
         flash(f"You aren't logged in", 'danger')
     return redirect(url_for('home'))
 
+
 def save_picture(form_picture):
     # generating random file name
     random_hex_filename = secrets.token_hex(8)
-    # spliting image into file name (not used later) and extension (f_ext)
+    # splitting image into file name (not used later) and extension (f_ext)
     _, file_ext = os.path.splitext(form_picture.filename)
     picture_filename = random_hex_filename + file_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_filename)
-    form_picture.save(picture_path)
+
+    # Using Pillow module to resize image
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
     return picture_filename
+
 
 @app.route('/account', methods=['GET', 'POST'])
 @login_required
@@ -102,3 +98,53 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', titler='Account', image_file=image_file, form=form)
+
+
+@app.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def create_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post has been created.', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form, legend='Create New Post')
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated.', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted.', 'success')
+    return redirect(url_for('home'))
