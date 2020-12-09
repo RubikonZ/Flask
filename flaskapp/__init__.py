a87a3dbab9d926d676e1d6f9b46ea4f7cc58ea73
Ruben <rubenzurr@gmail.com>
2020-12-09 15:33:33 +0300

    Added ability to change user password by sending email with token

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 # create and configure the app
@@ -13,5 +14,9 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+mail = Mail(app)
 
 from flaskapp import routes
